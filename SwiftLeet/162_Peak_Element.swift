a1d3f616c6ea55c58de1afa0eacee001447fb20b
Parthasarathy Gudivada <pgudivada@hotmail.com>
2025-06-09 09:39:03 -0400

    fixed the edge cases

--- SwiftLeet/162_Peak_Element.swift
+++ SwiftLeet/162_Peak_Element.swift
@@ -28,19 +28,33 @@ final class PeakElementSolution {
 
 final class ValleyElementSolution {
     func findValleyElement(_ nums: [Int]) -> Int {
-        var l = 0
-        var r = nums.count - 1
+        if nums.isEmpty {
+            return -1
+        }
 
-        while l < r {
-            let m = ( l + r ) / 2
+        let len = nums.count
+        if len == 1 {
+            return 0
+        }
 
-            if nums[m] <= nums[m + 1] {
-                r = m
+        if nums[0] < nums[1] { return 0 }
+        if nums[len - 1] < nums[len - 2] { return len - 1 }
+
+        var l = 1
+        var r = len - 2
+
+        while l <= r {
+            let m = (l + r) / 2
+
+            if nums[m - 1] > nums[m] && nums[m] < nums[m + 1] {
+                return m
+            } else if nums[m - 1] < nums[m] {
+                r = m - 1
             } else {
                 l = m + 1
             }
         }
 
-        return r
+        return -1
     }
 }
